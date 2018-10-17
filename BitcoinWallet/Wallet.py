ccb6f358172039d282d21e8b4484342f71bee5a5
Ankita Khatri <ankitakhatri58@gmail.com>
2018-10-16 22:42:35 -0700

    started code

---
+++ BitcoinWallet/Wallet.py
@@ -0,0 +1,16 @@
+def list():
+	#function to list all the public private key pairs
+	#print them by name
+
+def newKeyPair():
+	#function to generate a new key pair
+	PrivateKey()
+	PublicKey()
+
+def display(key):
+	#function to display a specific key
+	#planning to use QR code
+
+def base58Check():
+	#function to encode a base58 address
+	#code for this: https://github.com/bitcoin/bitcoin/blob/master/src/base58.h
