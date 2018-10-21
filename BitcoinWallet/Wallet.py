435719fcae661ead7bc3f3137498a0125b559c8d
Ankita Khatri <ankitakhatri58@gmail.com>
2018-10-21 15:18:33 -0700

    -work from Sunday 10/21

--- BitcoinWallet/Wallet.py
+++ BitcoinWallet/Wallet.py
@@ -1,16 +1,25 @@
+from Crypto.PublicKey import RSA
+
 def newKeyPair():
-	#function to generate a new key pair
-	#PrivateKey()
-	#PublicKey()
+    #function to generate a new key pair
+    key = RSA.generate(1024)
+    f = open("private.pem", "wb")
+    f.write(key.exportKey('PEM'))
+    f.close()
 
-def list():
-	#function to list all the public private key pairs
-	#print them by name
+    pubkey = key.publickey()
+    f = open("public.pem", "wb")
+    f.write(pubkey.exportKey('OpenSSH'))
+    f.close()
 
-def display(key):
-	#function to display a specific key
-	#planning to use QR code
-
-def base58Check():
-	#function to encode a base58 address
-	#code for this: https://github.com/bitcoin/bitcoin/blob/master/src/base58.h
+# def list():
+# 	#function to list all the public private key pairs
+# 	#print them by name
+#
+# def display(key):
+# 	#function to display a specific key
+# 	#planning to use QR code
+#
+# def base58Check():
+# 	#function to encode a base58 address
+# 	#code for this: https://github.com/bitcoin/bitcoin/blob/master/src/base58.h
