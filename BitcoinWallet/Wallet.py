d85d6bc57e2910a3ac9046c297987ddadb1b9a12
Ankita Khatri <ankitakhatri58@gmail.com>
2018-10-21 13:42:29 -0700

    updated to pycharm project

--- BitcoinWallet/Wallet.py
+++ BitcoinWallet/Wallet.py
@@ -1,11 +1,11 @@
+def newKeyPair():
+	#function to generate a new key pair
+	#PrivateKey()
+	#PublicKey()
+
 def list():
 	#function to list all the public private key pairs
 	#print them by name
-
-def newKeyPair():
-	#function to generate a new key pair
-	PrivateKey()
-	PublicKey()
 
 def display(key):
 	#function to display a specific key
