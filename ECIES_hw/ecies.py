59f3aa27500ede5eaaaf2d3919d9c439cd9d2661
Ankita Khatri <ankitakhatri58@gmail.com>
2018-11-06 18:28:28 -0800

    adding comments

--- ECIES_hw/ecies.py
+++ ECIES_hw/ecies.py
@@ -4,6 +4,10 @@ from base64 import b64decode
 from Crypto.Cipher import AES
 import sys
 
+'''
+Program to encrypt or decrypt a file using ECC key generator and AES Cipher Feedback Mode algorithm.
+'''
+
 #command line functionality
 #command line: python ./ecies.py e/d input.txt
 
@@ -14,13 +18,19 @@ file = sys.argv[2]
 with open(file, 'r') as myfile:
     input = myfile.read().replace('\n', ' ')
 
+#convert input from string to bytes
+data = bytes(input, 'utf-8')
+
 #generate key using ECC library
 key1 = ECC.generate(curve='P-256')
 
-data = bytes(input, 'utf-8')
+#print ECC key
+print(key1)
 
 #get private key in byte form
 key = bytes(str(key1.d), 'utf-8')
+
+#truncate to 16 bytes because AES-128 only accepts up to 16 bytes for a key
 key = key[:16]
 
 #initialize cipher and encrypt data (plaintext) to ciphertext
@@ -30,30 +40,35 @@ iv = b64encode(cipher.iv).decode('utf-8')
 ct = b64encode(ct_bytes).decode('utf-8')
 
 #decode iv and ct
-old_iv = b64decode(iv)
-old_ct = b64decode(ct)
-#initialize cipher and decrypt ciphertext to plaintext
-cipher = AES.new(key, AES.MODE_CFB, iv = old_iv)
-pt = cipher.decrypt(old_ct)
+decoded_iv = b64decode(iv)
+decoded_ct = b64decode(ct)
 
-#write to file ouput.txt
+#initialize cipher and decrypt ciphertext to plaintext
+cipher = AES.new(key, AES.MODE_CFB, iv = decoded_iv)
+pt = cipher.decrypt(decoded_ct)
+
+#open output.txt to write mode
 f = open("output.txt", "w")
 
+#write either encrypted or decrypted text based on command line argument
 if mode == 'e':
 
     #print encrypted text if mode is 'e'
     print("Encrypting file...")
     print("Encrypted text: ")
     print(ct)
+
+    #write to file
     f.write("Encrypted Text: ")
     f.write(str(ct))
 
 if mode == 'd':
 
-    #print decrypted text is mode is 'd'
+    #print decrypted text if mode is 'd'
     print("Decrypting file...")
     print("Decrypted text: ")
-    #print decrypted text
     print(pt)
+
+    #write to file
     f.write("Decrypted Text: ")
     f.write(str(pt))
